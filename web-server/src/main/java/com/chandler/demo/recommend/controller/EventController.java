96110d7967d6a7f8df2a0a6503772c28e33e38fb
Chandler <wzhao30@ford.com>
2022-05-25 13:57:58 +0800

    add mock user behavior data

--- web-server/src/main/java/com/chandler/demo/recommend/controller/EventController.java
+++ web-server/src/main/java/com/chandler/demo/recommend/controller/EventController.java
@@ -14,8 +14,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.Date;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.chandler.demo.recommend.model.SessionContainer.userTokenMap;
 
@@ -52,31 +52,63 @@ public class EventController {
 
     @GetMapping("/mock")
     public AjaxResult mock() {
-        for (int i = 0; i < 100; i++) {
-            UserBehaviorEntity entity = new UserBehaviorEntity();
-            String[] events = {"click", "NotInterested", "marked", "watch"};
-            entity.setEvent(events[(int) (Math.random() * events.length)]);
-            entity.setAgent("windows");
-            UserEntity userEntity = userRepository.findByName("test" + (int) (Math.random() * 1000));
-            entity.setUserId(userEntity.getId());
 
-            int movieId = (int) (Math.random() * 1200);
-            Optional<MovieEntity> movieEntityOptional = movieRepository.findById(movieId);
-            if (!movieEntityOptional.isPresent()) {
-                System.out.println(movieId + " 不存在");
-                continue;
-            }
-            MovieEntity movieEntity = movieEntityOptional.get();
-            entity.setMovieId(movieEntity.getId());
-
-            LocalDateTime localDateTime = LocalDateTime.now();
-            localDateTime.minusDays((int) (Math.random() * 60));
-            Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
-            entity.setTime(date);
-
-            userBehaviorRepository.save(entity);
-            System.out.println("插入成功: " + entity.toString());
+        //0.获取所有的movie到内存
+        Iterable<MovieEntity> iterable = movieRepository.findAll();
+        Iterator<MovieEntity> iterator = iterable.iterator();
+        ArrayList<MovieEntity> movieEntities = new ArrayList<>();
+        while (iterator.hasNext()) {
+            movieEntities.add(iterator.next());
         }
+
+        //1. 取出前的800个用户
+        for (int i = 0; i < 800; i++) {
+            String userName = "test" + i;
+            UserEntity userEntity = userRepository.findByName(userName);
+            String[] flags = userEntity.getMovieFlags().split(",");
+            //2. 选到当前的用户感兴趣的电影
+            List<MovieEntity> selectedMovies = movieEntities.stream().filter(e -> {
+                String[] movieCategories = e.getCategory().split(",");
+                boolean needed = false;
+                for (String flag : flags) {
+                    for (String movieFlag : movieCategories) {
+                        if (movieFlag.equalsIgnoreCase(flag)) {
+                            needed = true;
+                            break;
+                        }
+                    }
+                }
+                return needed;
+            }).collect(Collectors.toList());
+
+            //3. 每个用户随机对筛选出来的50部电影发生行为
+            List<UserBehaviorEntity> records = new ArrayList<>();
+            Set<Integer> handled = new HashSet<>();
+            while (handled.size() < Math.min(20, selectedMovies.size()-10)) {
+                MovieEntity movieEntity = selectedMovies.get((int) (Math.random() * selectedMovies.size()));
+                if (handled.contains(movieEntity.getId())) {
+                    continue;
+                }
+
+                UserBehaviorEntity entity = new UserBehaviorEntity();
+                String[] events = {"click", "NotInterested", "marked", "watch"};
+                entity.setEvent(events[(int) (Math.random() * events.length)]);
+                entity.setAgent("windows");
+                entity.setUserId(userEntity.getId());
+                entity.setMovieId(movieEntity.getId());
+
+                LocalDateTime localDateTime = LocalDateTime.now();
+                localDateTime.minusDays((int) (Math.random() * 60));
+                Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+                entity.setTime(date);
+
+                records.add(entity);
+                handled.add(movieEntity.getId());
+            }
+            userBehaviorRepository.saveAll(records);
+            System.out.println("处理完成用户: " + userName);
+        }
+
         return AjaxResult.success("处理成功");
     }
 
--- web-server/src/main/java/com/chandler/demo/recommend/dao/MovieRepository.java
+++ web-server/src/main/java/com/chandler/demo/recommend/dao/MovieRepository.java
@@ -1,7 +1,16 @@
 package com.chandler.demo.recommend.dao;
 
 import com.chandler.demo.recommend.entities.MovieEntity;
+import com.chandler.demo.recommend.entities.UserEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MovieRepository extends PagingAndSortingRepository<MovieEntity, Integer> {
+
+    @Query("select m from MovieEntity m where m.category like %?1%")
+    List<MovieEntity> findByFlag(@Param("name") String flag);
+
 }
